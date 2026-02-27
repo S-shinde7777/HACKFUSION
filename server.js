ec60b868d71ac7e600d78c6099929b4b5a76b4c3
Sagar Shinde <sagar.s.shinde7777@gmail.com>
2026-02-27 23:28:36 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors=require("cors");
+const path=require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname,"Frontend")));
 app.use(cors());
 app.use(express.json());
 
